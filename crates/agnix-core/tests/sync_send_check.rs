5855b52cb8464fe4d1df4cf7999bb8e163abc609
Avi Fenesh <aviarchi1994@gmail.com>
2026-02-05 02:38:06 +0200

    test: add Sync+Send compile-time checks for ValidatorRegistry and LintConfig

---
+++ crates/agnix-core/tests/sync_send_check.rs
@@ -0,0 +1,13 @@
+//! Test that ValidatorRegistry is Sync + Send
+
+fn assert_sync_send<T: Sync + Send>() {}
+
+#[test]
+fn test_validator_registry_is_sync_send() {
+    assert_sync_send::<agnix_core::ValidatorRegistry>();
+}
+
+#[test]
+fn test_lint_config_is_sync_send() {
+    assert_sync_send::<agnix_core::LintConfig>();
+}
